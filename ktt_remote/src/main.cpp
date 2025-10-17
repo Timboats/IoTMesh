cc3af6f80849da4cf08ee6df6223a111339d50c5
Timboats <72775377+Timboats@users.noreply.github.com>
2025-10-17 15:24:34 -0400

    Update main.cpp

--- ktt_remote/src/main.cpp
+++ ktt_remote/src/main.cpp
@@ -16,6 +16,10 @@
 #define SCREEN_WIDTH 128             // OLED display width, in pixels
 #define SCREEN_HEIGHT 64             // OLED display height, in pixels
 
+#define TFT_CS 13
+#define TFT_DC 33
+#define TFT_RST 32
+
 void setup()
 {
 
