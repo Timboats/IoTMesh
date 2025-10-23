8a68f724015f029e5022b3406eddfe7a562be66d
Timboats <72775377+Timboats@users.noreply.github.com>
2025-10-23 16:45:03 -0400

    Added config header file for all definitions

--- ktt_remote/src/main.cpp
+++ ktt_remote/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "config.h"
 
 // LIBRARIES
 #include "painlessMesh.h"      // Mesh WiFi
@@ -8,17 +9,6 @@
 #include "Adafruit_ILI9341.h"  // For TFT LCD Screen
 #include "ShiftIn.h"
 #include <math.h>
-
-// CONSTANTS
-#define MESH_PREFIX "IoTHub"     // Mesh WiFI name
-#define MESH_PASSWORD "IOAIHTHG"   // Mesh password
-#define MESH_PORT 5555         // Mesh port
-#define SCREEN_WIDTH 128             // OLED display width, in pixels
-#define SCREEN_HEIGHT 64             // OLED display height, in pixels
-
-#define TFT_CS 13
-#define TFT_DC 33
-#define TFT_RST 32
 
 void setup()
 {
