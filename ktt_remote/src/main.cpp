90ed4fff0c5bad519bff3232c6e4c8c034f1a60c
mtran07 <melissatran0125@gmail.com>
2026-03-01 15:21:40 -0500

    Update main.cpp

--- ktt_remote/src/main.cpp
+++ ktt_remote/src/main.cpp
@@ -1,13 +1,50 @@
-#include <Arduino.h>
-#include <WiFi.h>
+ktt_remote/src/main.cpp
 
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  WiFi.mode(WIFI_STA);
-  Serial.print("MAC Address: ");
-  Serial.println(WiFi.macAddress());
-}
+@@ -37,8 +37,8 @@ TFT_eSPI tft = TFT_eSPI();
 
-void loop() {}
-}
+// Global vars
+bool otaEnable = false;
+const int DEV_COUNT = 4;
+int selectedDevice = 1;
+int totalDevices = 0; // Mesh should still be stable with 10 devices connected
+int selectedValue = 150;
+uint8_t focusIndex = 0;
+uint8_t currentPage = NO_CONNECT_PAGE;
+
+@@ -46,7 +46,7 @@ uint8_t currentPage = NO_CONNECT_PAGE;
+unsigned long lastDebounce = 0; // for input buttons
+const unsigned long DEBOUNCE_DELAY = 200;
+
+int totalDevices = 0;
+
+
+
+void setup()
+
+
+@@ -149,14 +149,13 @@ void loop()
+    if(pressed(DPAD_UP) || pressed(DPAD_DOWN)) 
+    {
+      focusIndex = 1 - focusIndex;
+      //updateDeviceLine();
+      drawSendPage();
+    }
+    else if(pressed(DPAD_LEFT)) 
+    {
+      if(focusIndex == 0) 
+      {
+        selectedDevice = (selectedDevice + DEV_COUNT - 2) % DEV_COUNT + 1;
+        selectedDevice = (selectedDevice + totalDevices - 2) % totalDevices + 1;
+        drawSendPage();
+      } 
+      else 
+
+@@ -170,7 +169,7 @@ void loop()
+    {
+      if(focusIndex == 0) 
+      {
+        selectedDevice = selectedDevice % DEV_COUNT + 1;
+        selectedDevice = selectedDevice % totalDevices + 1;
+        drawSendPage();
+      } 
+      else 
