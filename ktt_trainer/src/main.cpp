494dcd27752f1e916418080c4d189f6709b23c27
mtran07 <melissatran0125@gmail.com>
2026-03-27 14:58:20 -0400

    Update remote trainer test

--- ktt_trainer/src/main.cpp
+++ ktt_trainer/src/main.cpp
@@ -2,13 +2,13 @@
 #include <WifiEspNow.h>
 #include <WiFi.h>
 
-// Data Structure
+// data structure
 typedef struct struct_message {
-  uint8_t  messageType;
-  int      tempo;
-  uint32_t timestamp;
-  uint8_t  targetDevice;
-  uint8_t  senderId;
+  uint8_t  messageType; // type of message    
+  int      tempo; // what tempo recieved from remote?
+  uint32_t timestamp;  // time stamp of when tempo was sent in millis
+  uint8_t  targetDevice; // target device sent from rmote 0 = everyone
+  uint8_t  senderId; // sender id of trainer 
 } struct_message;
 
 struct_message outgoingData;
@@ -18,10 +18,11 @@ void setup() {
   delay(1000);
 
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  Serial.print("MAC Address: ");
+  WiFi.disconnect(); // disconnect wifi to ensure no connection from external network
+  Serial.print("MAC Address: "); //print mac to verify correct tempo trainer device
   Serial.println(WiFi.macAddress());
 
+  // Condition to print if esp failed
   if (!WifiEspNow.begin()) {
     Serial.println("ERROR: WifiEspNow.begin() failed");
     while (1);
@@ -32,7 +33,7 @@ void setup() {
   uint8_t broadcastAddr[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
   WifiEspNow.addPeer(broadcastAddr);
 
-  Serial.println("Setup done, sending every 2 seconds...");
+  Serial.println("Setup done, will send every 2 seconds...");
 }
 
 void loop() {
@@ -45,7 +46,7 @@ void loop() {
   uint8_t broadcastAddr[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
   bool ok = WifiEspNow.send(broadcastAddr, (uint8_t*)&outgoingData, sizeof(outgoingData));
 
-  Serial.println(ok ? "→ Sent!" : "✗ Send failed");
+  Serial.println(ok ? " Sent!" : "Send failed");
 
   delay(2000);
 }
